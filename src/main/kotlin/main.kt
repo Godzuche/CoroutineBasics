a92f4ead4667dcbcf53177b81161006666a20339
Administrator <godxuche@gmail.com>
2021-10-28 04:02:37 +0100

    creating coroutines using Coroutine builders and suspend function for suspendable codes....

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,3 +1,22 @@
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 fun main(args: Array<String>) {
-    println("Hello World!")
+
+    runBlocking {
+        launch {
+            delayAndPrintln(500, "I'm the first println")
+        }
+        launch {
+            delayAndPrintln(1000, "I'm the middle println")
+        }
+        println("I'm the last println")
+    }
+
+}
+
+suspend fun delayAndPrintln(delay: Long, message: String) {
+    delay(delay)
+    println(message)
 }
